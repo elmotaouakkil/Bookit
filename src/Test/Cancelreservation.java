1d47ac44ddcf675e622ee424513fe1ac4e2c850b
elmotaouakkil <58572512+elmotaouakkil@users.noreply.github.com>
2020-02-29 15:48:43 -0500

    Update Cancelreservation.java

--- src/Test/Cancelreservation.java
+++ src/Test/Cancelreservation.java
@@ -3,6 +3,7 @@ package Test;
 public class Cancelreservation {
     public static void main(String[] args) {
         System.out.println("Cancel Reservation");
+        System.out.println("thnks");
 
     }
 }
